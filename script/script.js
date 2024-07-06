2498e658bf4c6086f4531770ed64f090f333605a
RenatoFringuello <renatofringuello@hotmail.it>
2024-07-06 12:16:37 +0200

    first commit

---
+++ script/script.js
@@ -0,0 +1,6 @@
+window.onload = function main(){
+    // la pagina è stata caricata
+
+    // imposto l'anno del credits
+    document.getElementById("credits").innerText += " " + new Date().getFullYear()
+}
