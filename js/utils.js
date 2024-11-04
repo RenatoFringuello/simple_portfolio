ac2c0d3e89b87574d13802c0d14b2d3817c2f0f6
RenatoFringuello <renatofringuello@hotmail.it>
2024-11-04 01:13:47 +0100

    add function to fetch json and inject projects card, add link style

--- js/utils.js
+++ js/utils.js
@@ -2,7 +2,19 @@
  * random not inclusive
  */
 export function getRandomInt(min, max) {
-    const minCeiled = Math.ceil(min);
-    const maxFloored = Math.floor(max);
-    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+    const minCeiled = Math.ceil(min)
+    const maxFloored = Math.floor(max)
+    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled)
+}
+
+/**
+ * fetch data JSON
+ */
+export async function fetchData(url) {
+    const requestURL = url
+    const request = new Request(requestURL)
+  
+    const response = await fetch(request)
+    const data = await response.json()
+    return data
 }
